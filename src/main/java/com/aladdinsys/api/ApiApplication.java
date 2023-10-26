ba2bd8ac82ce3c2af354e987504a567fa0efa98c
nadkaladdinsys <nadk@aladdinsys.co.kr>
2023-10-26 17:00:46 +0900

    최초 커밋 / PhysicalNamingStrategy 작업

---
+++ src/main/java/com/aladdinsys/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.aladdinsys.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@EnableScheduling
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
